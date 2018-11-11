f37a87101333fa008fa73e28817872b3d6d44d10
Janiot <Janiot@example.com>
2018-11-11 17:23:37 +0100

    added datatables

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('assets/', views.assets, name='assets'),
+    path('assets/<asset_id>/', views.asset, name = 'asset')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Asset
+
 def index2(request):
     return HttpResponse("Hello world! Data2m") 
 
@@ -8,3 +10,13 @@ def index2(request):
 def index(request):
     return render(request, 'core/index.html', {})
 
+
+def assets(request):
+    assets = Asset.objects.order_by('id')
+    context = {'assets': assets}
+    return render(request, 'core/assets.html', context)
+
+def asset(request, asset_id):
+    asset = Asset.objects.get(id=asset_id)   
+    context = {'asset': asset}
+    return render(request, 'core/asset.html', context)
